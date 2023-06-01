cc96b752cd0b4c0798039d53cb036444837e7bbe
Yuichi Ikeda <yuichii@outlook.com>
2023-06-01 19:12:07 +0900

    Update JSON data

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,12 @@
 module.exports = async function (context, req) {
-    context.res.json({
-        text: "Hello from the API"
-    });
+    
+    let responseJson = {
+        title: "ハンバーグ 160g",
+        picture: "hanbrug.jpg",
+        price: 1800,
+        description: "とてもジューシーなハンバーグです。",
+        calorie: 273
+    };
+    context.res.headers = { "Content-type" : "application/json" };
+    context.res.body = responseJson;
 };
