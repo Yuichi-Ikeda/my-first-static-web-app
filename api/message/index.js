c4ff4373bb86b623439ce19985255c3da8279fdd
Yuichi Ikeda <yuichii@outlook.com>
2023-06-01 18:07:26 +0900

    Update HTML

--- api/message/index.js
+++ api/message/index.js
@@ -1,5 +1,9 @@
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API"
+        title: "ハンバーグ 160g",
+        picture: "hamburger.jpg",
+        price: 1800,
+        description: "とてもジューシーなハンバーグです。",
+        calorie: 273
     });
 };
